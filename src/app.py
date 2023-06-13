9eac63ac2d2353814490ac8ad706dde6c117c045
Deimian Vásquez <deimianvasquez@gmail.com>
2023-06-12 21:02:07 -0400

    initial commmit

--- src/app.py
+++ src/app.py
@@ -14,36 +14,91 @@ from models import db, User
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 MIGRATE = Migrate(app, db)
+
 db.init_app(app)
 CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
+@app.route('/user/<user_id>', methods=['GET'])
+def handle_hello(user_id=None):
+    if request.method == "GET":
+        print(type(user_id))
+        if user_id is not None:
+            user = User()
+            user = user.query.get(user_id)
+
+            if user is not None:
+                return jsonify(user.serialize()), 200
+            else:
+                return jsonify({"message": "Not found"}), 404
+
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def all_users():
+    if request.method == 'GET':
+        users = User()
+        users = users.query.all()
+        users = list(map(lambda item: item.serialize(), users))
+        return jsonify(users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['POST'])
+def new_user():
+    data = request.json
+    if request.method == 'POST':
+        if data.get("name") is None:
+            return jsonify({"message": "wrong property"}), 400
+        if data.get("lastname") is None:
+            return jsonify({"message": "wrong property"}), 400
+        if data.get("email") is None:
+            return jsonify({"message": "wrong property"}), 400
+
+        user = User()
+        user_email = user.query.filter_by(email=data["email"]).first()
+
+        if user_email is None:
+            user = User(name=data["name"], lastname=data.get(
+                "lastname"), email=data.get("email"))
+            db.session.add(user)
+
+            try:
+                db.session.commit()
+                return jsonify(data), 201
+            except Exception as error:
+                print(error)
+                db.session.rollback()
+                return jsonify({"message": f"error {error.args}"}), 500
+
+        else:
+            return jsonify({"message": "user exist"}), 400
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f"<User {self.email}>"
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "create_at": self.created_at
         }
